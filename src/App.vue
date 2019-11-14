69f4fa1920bcbc34e24303537381215050297a8e
Balthazar <balthazar.pelletier@eemi.com>
2019-11-14 20:58:45 +0100

    add navbar in app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,42 @@
 <template>
-  <div>
-    <header>
+  <div id="app">
+    <header id="header__nav" class="nav-bars">
       <h1>Chef Cocktail</h1>
+      <router-link to="/">Home</router-link> |
     </header>
     <router-view />
   </div>
 </template>
 
 <style lang="scss">
+  #app {
+          font-family: 'Avenir', Helvetica, Arial, sans-serif;
+          -webkit-font-smoothing: antialiased;
+          -moz-osx-font-smoothing: grayscale;
+          text-align: center;
+          color: #2c3e50;
+
+          .nav-bars{
+              background: cornflowerblue;
+              position: fixed;
+              width: 100%;
+              padding: 10px;
+              color: white;
+
+              a {
+                  color: white;
+                  text-decoration: none;
+              }
+          }
+
+          #header-nav {
+              top: 0;
+              left: 0;
+          }
+
+          #footer-nav {
+              bottom: 0;
+              left: 0;
+          }
+      }
 </style>
